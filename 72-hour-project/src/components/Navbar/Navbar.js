4067f9da21cbcf8e7b33d2dac1af17b564fa71ff
Jacob Von Ogden <jvon1197@gmail.com>
2021-09-09 20:00:42 -0400

    merge attempt

---
+++ 72-hour-project/src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Ticketmaster from '../Ticketmaster/Ticketmaster';
+import Weather from '../Weather/Weather';
+import Nasa from '../Nasa/Nasa';
+
+
+
+const Navbar = (props) => {
+    
+    return(
+        
+        <div>
+            
+            <h1><strong>This will be where we link our components</strong></h1>
+            <Ticketmaster lat={props.lat} lon={props.lon}/>
+            <Weather lat={props.lat} lon={props.lon}/>
+            <Nasa lat={props.lat} lon={props.lon} />
+        
+        </div>
+    ) 
+}
+
+        
+
+export default Navbar;
