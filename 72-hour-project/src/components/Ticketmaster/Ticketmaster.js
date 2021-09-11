93e10a6579f280c1a56e80df4b2ad944eb9fde61
Shanna Ostermiller <shannaostermiller@gmail.com>
2021-09-11 01:07:38 -0400

    trying again

--- 72-hour-project/src/components/Ticketmaster/Ticketmaster.js
+++ 72-hour-project/src/components/Ticketmaster/Ticketmaster.js
@@ -1,42 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import Geohash from 'latlon-geohash';
-<<<<<<< HEAD
-=======
 
 
->>>>>>> a36a5aa9be9b6a3215de05d7eb09ac7f822389fd
+
 //location props passed as props.lat and props.lon
 const Ticketmaster = (props) => {
-<<<<<<< HEAD
+
     const baseURL = "https://app.ticketmaster.com/discovery/v2/events.json";
     const API_KEY = "H7Dt31l1jNxQDlKluqGGSlIE2GDEJjil";
     const [eventName, setEventName] = useState("");
     const [eventDate, setEventDate] = useState("");
-    const [eventVenue, setEventVenue] = useState("");
-=======
-    
-    const baseURL = "https://app.ticketmaster.com/discovery/v2/events.json";
-    const API_KEY = "H7Dt31l1jNxQDlKluqGGSlIE2GDEJjil";
+    const [eventVenue, setEventVenue] = useState("");git 
 
-    const [eventName, setEventName] = useState("");
-    const [eventDate, setEventDate] = useState("");
-    const [eventVenue, setEventVenue] = useState("");
 
->>>>>>> a36a5aa9be9b6a3215de05d7eb09ac7f822389fd
     useEffect(() => {
     const fetchEvents = async () => {
         const hash = Geohash.encode(props.lat, props.lon, 6);
         console.log(hash)
         let url = `${baseURL}?size=5&geoPoint=${hash}&apikey=${API_KEY}`;
-<<<<<<< HEAD
-        await fetch(url)
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            setEventName(data._embedded.events[0].name);
-            setEventDate(data._embedded.events[0].dates.start.localDate);
-            setEventVenue(data._embedded.events[0]._embedded.venues[0].name);
-=======
         
         await fetch(url)
           .then((res) => res.json())
@@ -50,18 +31,10 @@ const Ticketmaster = (props) => {
             
             setEventVenue(data._embedded.events[0]._embedded.venues[0].name);
           
->>>>>>> a36a5aa9be9b6a3215de05d7eb09ac7f822389fd
           });
       };
       fetchEvents();
     },[props.lat,props.lon])
-<<<<<<< HEAD
-    return(
-        <div>
-            <p>event: {eventName}</p>
-            <p>eventDate: {eventDate}</p>
-            <p>eventVenue: {eventVenue}</p>
-=======
     
     return(
     
@@ -72,7 +45,6 @@ const Ticketmaster = (props) => {
             <p>eventVenue: {eventVenue}</p>
             
         
->>>>>>> a36a5aa9be9b6a3215de05d7eb09ac7f822389fd
         </div>
     )
 }
